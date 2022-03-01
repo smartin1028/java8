125e25b4eeb3bb3d9ca882dbafa3a5858c49baea
dwsamba <leedw3232@gmail.com>
2022-03-01 12:30:18 +0900

    log test 02

--- java8_maven/src/main/java/com/ex01/MyTest.java
+++ java8_maven/src/main/java/com/ex01/MyTest.java
@@ -5,13 +5,25 @@ import org.apache.logging.log4j.Logger;
 
 public class MyTest {
 
-
-	private static final Logger logger00 = LogManager.getLogger(MyTest.class);
-	private static final Logger logger01 = LogManager.getLogger(MyTest.class.getName());
-	private static final Logger logger02 = LogManager.getLogger();
-
+	private final Logger logger00 = LogManager.getLogger(MyTest.class);
+	private final Logger logger01 = LogManager.getLogger(MyTest.class.getName());
+	private final Logger logger02 = LogManager.getLogger();
 
 	public static void main(String[] args) {
+
+		new MyTest();
+
+	}
+
+	public MyTest(){
+		int i = 10;
+		while (i != 0){
+			i--;
+			myPrint();
+		}
+	}
+
+	private void myPrint() {
 		logger00.error("logger00 Hello, World!");
 		logger01.error("logger01 Hello, World!");
 		logger02.info("logger02 Hello, World!");
@@ -22,6 +34,7 @@ public class MyTest {
 		logger00.warn("We've just greeted the user!");
 		logger00.error("We've just greeted the user!");
 		logger00.fatal("We've just greeted the user!");
+
 	}
 
 }
